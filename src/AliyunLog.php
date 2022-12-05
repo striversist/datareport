6c887de5053b1ece65207b84de02b817909e4e40
chengchao <chengchao@cashcashpro.id>
2022-12-05 17:20:24 +0800

    日志上报增加哥伦比亚配置

--- src/AliyunLog.php
+++ src/AliyunLog.php
@@ -85,6 +85,11 @@ class AliyunLog
                 'project_name' => 'data-la',
                 'log_store'    => 'cashcash',
             ],
+            'co' => [
+                'end_point'    => $linkType == 0 ? 'us-west-1-intranet.log.aliyuncs.com' : 'us-west-1.log.aliyuncs.com',
+                'project_name' => 'data-co',
+                'log_store'    => 'cashcash',
+            ],
         ];
         return $log_arr[$countryCode];
     }
--- src/ReportClient.php
+++ src/ReportClient.php
@@ -26,6 +26,7 @@ class ReportClient
     const REPORT_AREA_TH = 3;//上报地区：泰国
     const REPORT_AREA_MX = 4;//上报地区：墨西哥
     const REPORT_AREA_LA = 5;//上报地区：老挝
+    const REPORT_AREA_CO = 6;//上报地区：哥伦比亚
 
     const USER_DEVICE   = 'report/stat/userdevice'; //新增设备信息
     const USER_ACTIVE   = 'report/stat/useractive'; //日活数据
