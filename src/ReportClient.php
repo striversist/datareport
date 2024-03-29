5f72104ff8b0fcc49301c36231b781f91383a2f8
leizhenyu <907602907@qq.com>
2024-03-29 16:22:22 +0800

    feat: 客户工单校验

--- src/ReportClient.php
+++ src/ReportClient.php
@@ -100,6 +100,7 @@ class ReportClient
     //印度服务-业务上报
     const NAME_CHECK_IN   = 'report/stat/namecheck'; // 姓名一致性校验-业务上报
     const BANK_CHECK_IN    = 'report/stat/bankcheck'; //印度银行卡校验-业务上报
+    const CUSTOMER_TICKET_CHECK = 'report/service/customerticketcheck';  // 客户工单校验
 
     /**
      * [__construct description]
@@ -2572,6 +2573,39 @@ class ReportClient
         $data = array_merge($data, $this->reportData);
         // 离线数据存储
         $this->offlineProcess->addLog(self::IP_CHECK, $data);
+        return true;
+    }
+
+    /**
+     * 客户工单检测
+     * @param $app_package
+     * @param $offer_package
+     * @param $channel_type
+     * @param $is_pay
+     * @param int $country_code
+     * @param string $request_id
+     * @param string $request_time
+     * @return bool
+     */
+    public function customerTicketCheck($app_package, $offer_package, $channel_type, $is_pay, $country_code = self::REPORT_AREA_ID, $request_id = '', $request_time = '')
+    {
+        $res = $this->getDateDetail($request_time);
+        $data = array(
+            'app_package' => $app_package,
+            'offer_package' => $offer_package,
+            'channel_type' => $channel_type,
+            'is_pay' => $is_pay,
+            'country_code' => $country_code,
+            'create_time' => time(),
+            'request_id' => $request_id,
+            'report_year' => $res['year'],
+            'report_month' => $res['month'],
+            'report_day' => $res['day'],
+            'report_time' => $res['time'],
+        );
+        $data = array_merge($data, $this->reportData);
+        // 离线数据存储
+        $this->offlineProcess->addLog(self::CUSTOMER_TICKET_CHECK, $data);
         return true;
     }
 }
