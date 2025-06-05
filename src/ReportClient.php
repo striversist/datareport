e72b19dbc82af4ff796c28b719e8bab240680f20
leizhenyu <907602907@qq.com>
2025-06-05 18:38:49 +0800

    feat: 增加ai消息对话数据上报

--- src/ReportClient.php
+++ src/ReportClient.php
@@ -101,6 +101,7 @@ class ReportClient
     const EXPRESSION_CHECK = 'report/service/expressioncheck'; //异常表情检测
     const DEEP_FAKE_CHECK = 'report/service/deepfakecheck'; //人脸造假检测
     const H5_LIVENESS_CHECK = 'report/service/h5liveness'; //H5活体
+    const AI_CHAT_COMPLETION = 'report/service/aichatcompletion'; //ai消息对话
     //印度服务
     const NAME_CHECK   = 'report/service/namecheck'; // 姓名一致性校验
     const BANKCHECK    = 'report/service/bankcheck'; //印度银行卡校验
@@ -2736,6 +2737,47 @@ class ReportClient
         $data = array_merge($data, $this->reportData);
         // 离线数据存储
         $this->offlineProcess->addLog(self::H5_LIVENESS_CHECK, $data);
+        return true;
+    }
+
+    /**
+     * ai消息对话
+     * @param $app_package
+     * @param $offer_package
+     * @param $model_id
+     * @param $prompt_tokens
+     * @param $completion_tokens
+     * @param $total_tokens
+     * @param $channel_type
+     * @param $is_pay
+     * @param int $country_code
+     * @param string $request_id
+     * @param string $request_time
+     * @return bool
+     */
+    public function aiChatCompletion($app_package, $offer_package, $model_id, $prompt_tokens, $completion_tokens, $total_tokens, $channel_type, $is_pay, $country_code = self::REPORT_AREA_ID, $request_id = '', $request_time = '')
+    {
+        $res = $this->getDateDetail($request_time);
+        $data = array(
+            'app_package' => $app_package,
+            'offer_package' => $offer_package,
+            'model_id' => $model_id,
+            'prompt_tokens' => $prompt_tokens,
+            'completion_tokens' => $completion_tokens,
+            'total_tokens' => $total_tokens,
+            'channel_type' => $channel_type,
+            'is_pay' => $is_pay,
+            'country_code' => $country_code,
+            'create_time' => time(),
+            'request_id' => $request_id,
+            'report_year' => $res['year'],
+            'report_month' => $res['month'],
+            'report_day' => $res['day'],
+            'report_time' => $res['time']
+        );
+        $data = array_merge($data, $this->reportData);
+        // 离线数据存储
+        $this->offlineProcess->addLog(self::AI_CHAT_COMPLETION, $data);
         return true;
     }
 }
